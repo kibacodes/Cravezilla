a7f58a1fa5095768dea1f01fd8bacb2d1226ee35
Prashant Chakraborty <prashantchakraborty@Prashants-MacBook-Pro.local>
2025-07-09 22:14:43 +0530

    CraveZilla Setup 02

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-/**
- *
- * <div>
- *      <div>
- *          h1
- *
- *      </div>
- *
- *
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "This is CraveZilla 🚀"),
+    React.createElement("h2", {}, "by Prashant Chakraborty"),
   ]),
 
   React.createElement("div", { id: "child2" }, [
@@ -23,8 +12,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 ]);
-
-// JSX
 
 console.log(parent); // object
 
