7ba166d1250e7bece7d1ae265a6bab95f4ff4637
Prashant Chakraborty <prashantchakraborty@Prashants-MacBook-Pro.local>
2025-07-24 21:05:00 +0530

    CraveZilla component setup

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is CraveZilla 🚀"),
-    React.createElement("h2", {}, "by Prashant Chakraborty"),
-  ]),
+// React Element
+// const jsxHeading = (
+//   <h1 id="heading" tabIndex="5">
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
 
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+const spanElem = <span>React Element</span>;
 
-console.log(parent); // object
+const title = (
+  <h1 id="heading" tabIndex="5">
+    {spanElem}
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+// React Functional Component
+// Lines 14-22 do the same thing but written differently.
+
+// const HeadingComponent = () => {
+//   return <h1> Namaste React Functional Component</h1>
+// };
+
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <h1 className="heading"> Namaste React Functional Component </h1>
+  </div>
+);
+
+// const HeadingComponent = <h1>Namaste React Functional Component</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// it will replace everything that is inside the root in the HTML file
 
-root.render(parent);
+// React Element vs React Component: Difference in Rendering
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
