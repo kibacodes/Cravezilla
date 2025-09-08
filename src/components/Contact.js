3d1295dfe8b6746693cf60e0d4487f0b3eed3d95
Prashant Chakraborty <prashantchakraborty@Prashants-MacBook-Pro.local>
2025-09-09 00:13:06 +0530

    Add React-Router to CraveZilla

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <h2>For any problems, please contact us at +91 9123456789</h2>
+        </div>
+    );
+};
+
+export default Contact;
